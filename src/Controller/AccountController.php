2fbfea0ccd31392707f29c86240aae686ca30e24
Adrien <adrien.dacunto@limayrac.fr>
2021-12-28 16:32:10 +0100

    v0.3 - Planning Semaine

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Account;
 use App\Form\AccountFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,15 +14,23 @@ class AccountController extends AbstractController
     /**
      * @Route("/compte/create", name="compte_create")
      */
-    public function new()
+    public function new(Request $request)
     {
-        $article = new Account();
-        $form = $this->createForm(AccountFormType::class, $article);
+        $account = new Account();
+        $form = $this->createForm(AccountFormType::class, $account);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form ->isValid()){
+            $account = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($account);
+            $entityManager->flush();
+            return $this->redirectToRoute('newIntervenant_success');
+        }
 
         return $this->render('compte/new.html.twig', [
             'form' => $form->createView()
         ]);
     }
 
-    // ...
 }
--- src/Form/AccountFormType.php
+++ src/Form/AccountFormType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Account;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -24,6 +25,7 @@ class AccountFormType extends AbstractType
             ])
             ->add('account_firstname', TextType::class)
             ->add('account_nam', TextType::class)
+            ->add('Enregistrer', SubmitType::class)
         ;
     }
 
