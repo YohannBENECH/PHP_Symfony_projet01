6deb15401db4110ee8f43c3f74da01f01aa98fa4
Adrien <adrien.dacunto@limayrac.fr>
2022-01-13 12:08:47 +0100

    v0.4 - Planning Semaine: Matiere formulaire + Intervenant formulaire

---
+++ src/Entity/Matiere.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MatiereRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MatiereRepository::class)
+ */
+class Matiere
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $specialite;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSpecialite(): ?bool
+    {
+        return $this->specialite;
+    }
+
+    public function setSpecialite(bool $specialite): self
+    {
+        $this->specialite = $specialite;
+
+        return $this;
+    }
+}
